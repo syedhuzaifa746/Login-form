ffc714dc4cfa28ffc4c5405acd445a1fc2b93791
syedhuzaifa746 <107565690+syedhuzaifa746@users.noreply.github.com>
2023-06-17 15:43:13 +0500

    update app.js

--- app.js
+++ app.js
@@ -5,26 +5,32 @@ let getPassword;
 
 function signup() {
     getUsername = document.getElementById("getUsername").value;
+    getUsername = getUsername.trim();
     getPassword = document.getElementById("getPassword").value;
-    console.log(getUsername, ' value 1')
-    console.log(getPassword, ' value 2')
+    getPassword = getPassword.trim();
+    if( getUsername != "" && getPassword != ""){
     localStorage.setItem("getUsername", getUsername);
     localStorage.setItem("getPassword", getPassword);
     window.location.href = "index.html"
+    }else{
+        alert("Enter valid username and password")
+    }
+    
 }
-
-
 
 function login() {
     let username = document.getElementById("username").value;
+    username = username.trim();
     let password = document.getElementById("password").value;
+    password = password.trim();
     let storedUsername = localStorage.getItem("getUsername")
     let storedPassword = localStorage.getItem("getPassword")
     console.log('success')
 
-    if (username == storedUsername && password == storedPassword) {
+    if (username == storedUsername &&
+         password == storedPassword
+         && username!= "" && password !=""){
         window.location.href = "home.html"
-
     } else {
         alert('Enter correct Username and Password')
     }
